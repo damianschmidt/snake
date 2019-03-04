c0ddd53eda6935914dd4961e624422c70ed1ed58
Damian Schmidt <damian.schmidt97@gmail.com>
2019-03-05 00:51:54 +0100

    Add reaction on key press and game over screen

--- game.py
+++ game.py
@@ -1,8 +1,9 @@
 import pygame
-import math
+from math import ceil
 from random import randint
 
-RECT_COLOR = (0, 220, 0)
+FOOD_COLOR = (220, 0, 0)
+SNAKE_COLOR = (0, 220, 0)
 BG_COLOR = (50, 50, 50)
 RECT_DIM = 10
 SCREEN_WIDTH = 600
@@ -15,6 +16,14 @@ pygame.init()
 game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Snake')
 clock = pygame.time.Clock()
+
+
+def add_text(text, font, size, color, width, height):
+    font = pygame.font.SysFont(font, size)
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.center = (width, height)
+    game_screen.blit(text_surface, text_rect)
 
 
 class Snake(object):
@@ -37,37 +46,52 @@ class Snake(object):
     def head(self):
         return self.body_list[-1]
 
+    def game_over(self):
+        add_text('GAME OVER', 'Arial', 72, (255, 255, 255), SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2.2)
+        add_text('YOUR SCORE: ' + str(self.score), 'Arial', 36, (255, 255, 255), SCREEN_WIDTH / 2, SCREEN_HEIGHT / 1.8)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+            pygame.display.update()
+
     def move_up(self):
         self.head.y -= RECT_DIM
         if self.head.y < 0:
-            print('Out of area!')
+            self.game_over()
 
     def move_down(self):
         self.head.y += RECT_DIM
         if self.head.y > SCREEN_HEIGHT:
-            print('Out of area!')
+            self.game_over()
 
     def move_left(self):
         self.head.x -= RECT_DIM
-        if self.head.x < SCREEN_WIDTH:
-            print('Out of area!')
+        if self.head.x < 0:
+            self.game_over()
 
     def move_right(self):
         self.head.x += RECT_DIM
         if self.head.x > SCREEN_WIDTH:
-            print('Out of area!')
+            self.game_over()
 
     def update_snake(self):
         self.body_list.pop(0)
         new_part = Body(self.body_list[-1].x, self.body_list[-1].y)
         self.body_list.insert(-1, new_part)
-
         self.directions[self.direct]()
+
+    def change_direct(self, direct):
+        if direct is not self.direct:
+            self.direct = direct
 
     def render(self):
         for body_part in self.body_list:
             body_rect = pygame.Rect(body_part.x, body_part.y, RECT_DIM, RECT_DIM)
-            pygame.draw.rect(game_screen, RECT_COLOR, body_rect)
+            pygame.draw.rect(game_screen, SNAKE_COLOR, body_rect)
 
 
 class Body(object):
@@ -78,12 +102,12 @@ class Body(object):
 
 class Food(object):
     def __init__(self):
-        self.x = int(math.ceil((randint(0, SCREEN_WIDTH - RECT_DIM))) / 10.0) * 10
-        self.y = int(math.ceil((randint(0, SCREEN_HEIGHT - RECT_DIM))) / 10.0) * 10
+        self.x = int(ceil((randint(0, SCREEN_WIDTH - RECT_DIM))) / 10.0) * 10
+        self.y = int(ceil((randint(0, SCREEN_HEIGHT - RECT_DIM))) / 10.0) * 10
         self.food_rect = pygame.Rect(self.x, self.y, RECT_DIM, RECT_DIM)
 
     def render(self):
-        pygame.draw.rect(game_screen, RECT_COLOR, self.food_rect)
+        pygame.draw.rect(game_screen, FOOD_COLOR, self.food_rect)
 
 
 def game_loop():
@@ -95,6 +119,9 @@ def game_loop():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+
+            if event.type == pygame.KEYDOWN:
+                snake.direct = event.key
 
         game_screen.fill(BG_COLOR)
         food.render()
