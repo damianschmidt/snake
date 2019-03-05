253c4a547662e14e9ca713ae4ab08aa1bd5767ae
Damian Schmidt <damian.schmidt97@gmail.com>
2019-03-05 16:27:55 +0100

    Add eating food

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ from random import randint
 FOOD_COLOR = (220, 0, 0)
 SNAKE_COLOR = (0, 220, 0)
 BG_COLOR = (50, 50, 50)
-RECT_DIM = 10
+RECT_DIM = 30
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 SNAKE_INIT_LENGTH = 5
@@ -50,13 +50,34 @@ class Snake(object):
         add_text('GAME OVER', 'Arial', 72, (255, 255, 255), SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2.2)
         add_text('YOUR SCORE: ' + str(self.score), 'Arial', 36, (255, 255, 255), SCREEN_WIDTH / 2, SCREEN_HEIGHT / 1.8)
 
+        x_button = 200
+        y_button = 380
+        width_button = 200
+        height_button = 50
+        button_color = (0, 0, 200)
+        button_color_hover = (0, 0, 240)
+
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
 
+            mouse = pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed()
+
+            if x_button + width_button > mouse[0] > x_button and y_button + height_button > mouse[1] > y_button:
+                pygame.draw.rect(game_screen, button_color_hover, (x_button, y_button, width_button, height_button))
+                if click[0] == 1:
+                    game_loop()
+            else:
+                pygame.draw.rect(game_screen, button_color, (x_button, y_button, width_button, height_button))
+
+            add_text('RESTART', 'Arial', 24, (255, 255, 255), x_button + (width_button / 2),
+                     y_button + (height_button / 2))
+
             pygame.display.update()
+            clock.tick(15)
 
     def move_up(self):
         self.head.y -= RECT_DIM
@@ -77,6 +98,17 @@ class Snake(object):
         self.head.x += RECT_DIM
         if self.head.x > SCREEN_WIDTH:
             self.game_over()
+
+    def eat_food(self, food):
+        food.reset(self)
+        self.score += 1
+        eat_part = self.head
+        self.body_list.insert(0, eat_part)
+
+    def collision(self, food):
+        # with food
+        if self.head.x == food.x and self.head.y == food.y:
+            self.eat_food(food)
 
     def update_snake(self):
         self.body_list.pop(0)
@@ -102,8 +134,18 @@ class Body(object):
 
 class Food(object):
     def __init__(self):
-        self.x = int(ceil((randint(0, SCREEN_WIDTH - RECT_DIM))) / 10.0) * 10
-        self.y = int(ceil((randint(0, SCREEN_HEIGHT - RECT_DIM))) / 10.0) * 10
+        self.x = int(ceil((randint(0, SCREEN_WIDTH - RECT_DIM))) / float(RECT_DIM)) * RECT_DIM
+        self.y = int(ceil((randint(0, SCREEN_HEIGHT - RECT_DIM))) / float(RECT_DIM)) * RECT_DIM
+        self.food_rect = pygame.Rect(self.x, self.y, RECT_DIM, RECT_DIM)
+
+    def reset(self, snake):
+        self.x = int(ceil((randint(0, SCREEN_WIDTH - RECT_DIM))) / float(RECT_DIM)) * RECT_DIM
+        self.y = int(ceil((randint(0, SCREEN_HEIGHT - RECT_DIM))) / float(RECT_DIM)) * RECT_DIM
+
+        for body_part in snake.body_list:
+            if self.x == body_part.x and self.y == body_part.y:
+                self.reset(snake)
+
         self.food_rect = pygame.Rect(self.x, self.y, RECT_DIM, RECT_DIM)
 
     def render(self):
@@ -127,6 +169,7 @@ def game_loop():
         game_screen.fill(BG_COLOR)
         food.render()
         snake.render()
+        snake.collision(food)
         snake.update_snake()
         pygame.display.update()
         clock.tick(15)
